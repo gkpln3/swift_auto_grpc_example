e764411e9c3a140881c49b2c57f6807d4c84bb70
Guy Kaplan <gkpln3@gmail.com>
2021-05-24 15:48:12 +0300

    Initial commit

---
+++ Podfile
@@ -0,0 +1,10 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'swift_auto_grpc_example' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+  pod 'gRPC-Swift', '~> 1.0.0'
+  pod 'SwiftProtobuf', '~> 1.0'
+  pod 'gRPC-Swift-Plugins'
+end
--- swift_auto_grpc_example/ViewController.swift
+++ swift_auto_grpc_example/ViewController.swift
@@ -11,9 +11,11 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let message = Example_DoSomethingRequest.with {
+            $0.reqString = "Hello, world!"
+        }
+        print(message.reqString)
     }
-
-
 }
 
