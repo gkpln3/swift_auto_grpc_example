46a8f09263a096e50b006245f97659e9e31c10b8
Guy Kaplan <gkpln3@gmail.com>
2021-05-24 15:29:10 +0300

    Initial Commit

---
+++ swift_auto_grpc_example/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  swift_auto_grpc_example
+//
+//  Created by Guy Kaplan on 24/05/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
